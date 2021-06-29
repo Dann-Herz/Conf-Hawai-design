52cca9016ba0ed1809859f0c27627358809cf9bf
Dann Herz <danielhdzmzin@gmail.com>
2021-06-29 03:31:36 -0500

    I change the bootstrap version and the file used, from bootstrap.css to bootstrap.min.css

--- src/App.js
+++ src/App.js
@@ -5,33 +5,31 @@ import './App.css';
 function App() {
   return (
     <div>
-      <div className="container">
-        <div className="row pt-4">
-            <div className="col"></div>
-            <div className="col"></div>
-            <div className="col"></div>
+      <footer id="footer">
+        <div className="container">
+          <div className="row text-center">
+            <div className="col-12 col-lg">
+              <a href="#">Preguntas Frecuentes</a>
+            </div>
+            <div className="col-12 col-lg">
+              <a href="#">Contactanos</a>
+            </div>
+            <div className="col-12 col-lg">
+              <a href="#">Prensa</a>
+            </div>
+            <div className="col-12 col-lg">
+              <a href="#">Conferencias</a>
+            </div>
+            <div className="col-12 col-lg">
+              <a href="#">Términos y condiciones</a>
+            </div>
+            <div className="col-12 col-lg">
+              <a href="#">Estudiantes</a>
+            </div>
+          </div>
         </div>
-        <div className="row">
-            <div className="col"></div>
-            <div className="col"></div>
-            <div className="col"></div>
-            <div className="col"></div>
-        </div>
-        <div className="row">
-            <div className="col"></div>
-            <div className="col"></div>
-        </div>
-        <div className="row">
-            <div className="col"></div>
-            <div className="col"></div>
-            <div className="col"></div>
-            <div className="col"></div>
-            <div className="col"></div>
-            <div className="col"></div>
-            <div className="col"></div>
-            <div className="col"></div>
-        </div>
-      </div>
+      </footer>
+
     </div>
   );
 }
